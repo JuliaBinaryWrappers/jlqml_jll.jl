7ccc152e0850a26605695d262ff241116df75db3
jlbuild <juliabuildbot@gmail.com>
2023-03-06 17:22:15 +0000

    jlqml_jll build 0.5.2+0

--- src/wrappers/x86_64-apple-darwin-julia_version+1.7.0.jl
+++ src/wrappers/x86_64-apple-darwin-julia_version+1.7.0.jl
@@ -5,12 +5,12 @@ using libcxxwrap_julia_jll
 using Qt6Declarative_jll
 using Qt6Svg_jll
 JLLWrappers.@generate_wrapper_header("jlqml")
-JLLWrappers.@declare_library_product(libjlqml, "@rpath/libjlqml.0.5.1.dylib")
+JLLWrappers.@declare_library_product(libjlqml, "@rpath/libjlqml.0.5.2.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(libcxxwrap_julia_jll, Qt6Declarative_jll, Qt6Svg_jll)
     JLLWrappers.@init_library_product(
         libjlqml,
-        "lib/libjlqml.0.5.1.dylib",
+        "lib/libjlqml.0.5.2.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
